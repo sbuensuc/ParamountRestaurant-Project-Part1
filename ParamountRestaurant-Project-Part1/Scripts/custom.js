8d44c3d7c00045e6c542cf3c6fbc74bb08bf0716
Sam <Sam@DESKTOP-O0S9A91>
2016-12-02 18:44:56 -0500

    Add project files.

---
+++ ParamountRestaurant-Project-Part1/Scripts/custom.js
@@ -0,0 +1,14 @@
+﻿//Filename: custom.js
+//Author Name: Sam Buensuceso
+//Website Name: http://paramount-bistro.azurewebsites.net/
+//File Description: custom js 
+
+//Custom JS code
+
+//JS to handle image modal
+$(function () {
+    $('.pop').on('click', function () {
+        $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+        $('#imagemodal').modal('show');
+    });
+});
---
+++ ParamountRestaurant-Project-Part1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ParamountRestaurant_Project_Part1.Startup))]
+namespace ParamountRestaurant_Project_Part1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
